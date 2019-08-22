c5895b61cefebe642a906b1ea99cb4c0e264eb11
Liam Fisher <liamfisher1@gmail.com>
2019-08-22 17:36:13 +0100

    Added script to create symlinks

---
+++ Set-VScriptSymLinks.ps1
@@ -0,0 +1,23 @@
+#Requires -RunAsAdministrator
+
+#worksonmymachine tm
+$vscriptPath = "D:\SteamLibrary\steamapps\common\left 4 dead 2\left4dead2\scripts\vscripts"
+
+# Remove old symlinks
+Get-ChildItem -Path $vscriptPath | Where-Object {
+    $_.Mode -eq '-a---l' -and
+    $_.Extension -eq '.nut' -and
+    (Split-Path -Path $_.Target) -eq (Join-Path -Path $PSScriptRoot -ChildPath "vscripts")
+} | Remove-Item -Force
+
+return
+
+Get-ChildItem -Path $PSScriptRoot\vscripts | ForEach-Object {
+    $splat = @{
+        Path = Join-Path -Path $vscriptPath -ChildPath $_.Name
+        ItemType = "SymbolicLink"
+        Value = $_.FullName
+        Force = $true
+    }
+    New-Item @splat
+}
